bb12b05e0693b57fff7e11f46bf17f53dd8d20c7
Derick <derick290801@gmail.com>
2022-06-27 23:23:06 -0400

    Starting application

---
+++ main.py
@@ -0,0 +1,12 @@
+from Range_oduranc import Range
+
+print("1. GetAllPoints." + 
+      "\n2. GetEndPoints." + 
+      "\n3. Contains." + 
+      "\n4. ContainsRange")
+
+option = input("Seleccione la operación que desea realizar")
+
+
+# # R1 = Range('[2,6)')
+# print(R1.getAllPoints())
